a97889811d1eaca0db8d06deef106b5a91a0030b
bbittiker1 <bbittiker@gmail.com>
2020-05-12 20:19:19 -0700

    simplify timer

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -45,8 +45,6 @@ const useStyles = makeStyles(theme => ({
 }));
 
 
-
-
 export default function Dashboard() {
 	const classes = useStyles(theme);
 
@@ -61,108 +59,6 @@ export default function Dashboard() {
 	const [loading, setLoading] = useState( true);
 	const [seconds, setSeconds] = useState( 0);
 
-	// let { roundTimers, totalSeconds } = loadTimers(numRounds, breakSeconds, roundSeconds);
-
-	// useEffect(() => {
-	// 	navigator.geolocation.getCurrentPosition(
-	// 		position => {
-	// 			const { latitude, longitude } = position.coords;
-	// 			setUserLocation({ lat: latitude, lng: longitude });
-	// 			setLoading(false);
-	// 			setMapUrl(`https://maps.googleapis.com/maps/api/staticmap?center=${userLocation.lat},${userLocation.lng}&zoom=15&size=400x400&maptype=roadmap&key=${Config.googleMapsApiKey}&markers=color:red%7Clabel:You%7C${userLocation.lat},${userLocation.lng}`);
-	// 			setLoading(false);
-	// 		},
-	// 		() => {
-	// 			setLoading(false);
-	// 		}
-	// 	);
-	// }, [loading, userLocation, mapUrl]);
-
-	// console.log(totalSeconds);
-	// console.log(roundTimers);
-
-
-
-	const translateSeconds = (seconds) => {
-		const mins = Math.floor(seconds / 60).toFixed(0);
-		const _seconds = seconds % 60;
-
-		console.log(`${mins < 10 ? "0" + mins : mins}:${_seconds < 10 ? "0" + _seconds : _seconds}`);
-		// setSeconds(`${mins < 10 ? "0" + mins : mins}:${_seconds < 10 ? "0" + _seconds : _seconds}`);
-
-		// console.log(seconds);
-		//
-		return seconds;
-	};
-
-	class Countdown {
-		constructor(options) {
-			this.seconds = options.seconds;
-			this.updateStatus = options.onUpdateStatus;
-			this.counterEnd = options.onCounterEnd;
-			this.timer = {};
-			this._instance = this;
-		}
-
-		decrementCounter() {
-			this.updateStatus(this.seconds);
-
-			if (this.seconds === 0) {
-				this.counterEnd();
-				this.stop();
-			}
-
-			this.seconds--;
-		}
-
-		start() {
-			clearInterval(this.timer);
-			this.timer = 0;
-			// seconds = options.seconds;
-			this.timer = setInterval( () => {
-				this.decrementCounter();
-			}, 1000);
-		}
-
-		stop() {
-			if(this.seconds > 0) {
-				console.log("paused.");
-			}
-
-			clearInterval(this.timer);
-		};
-	}
-
-	const timers = [];
-
-	const nextRound = () => {
-		if(timers.length > 0) {
-			return timers.shift().start();
-		} else {
-			console.log("fight ended.");
-		}
-	};
-
-	let x = new Countdown({
-		seconds: 10,  // number of seconds to count down
-		onUpdateStatus: translateSeconds,
-		onCounterEnd: () => {
-			console.log("round ended");
-			nextRound();
-		}
-	});
-
-	let y = new Countdown({
-		seconds: 5,  						// number of seconds to count down
-		onUpdateStatus: translateSeconds, 	// callback for each second
-		onCounterEnd: () => {
-			nextRound();
-		}
-	});
-
-	timers.push(x);
-	timers.push(y);
-
 	return (
 		<Grid className={useStyles.paper} style={{border: 'blue 1px solid'}}>
 			<Grid item xs={12}>
@@ -172,7 +68,7 @@ export default function Dashboard() {
 				<Typography component="h5" variant="h5">{numRounds} Rounds</Typography>
 			</Grid>
 			<Grid item xs={12}>
-				<RoundTimer timers={timers}/>
+				<RoundTimer />
 			</Grid>
 			{/*<Grid item xs={12}>*/}
 			{/*	<Typography component="h6" variant="h6">{ `Rounds ${ translateSeconds(roundSeconds)} / Notice ${roundNotifySeconds}s` }</Typography>*/}
